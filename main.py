de94bef7a40dd7391a3e33af13d666e728ab8a8c
TMCSEHM <92201012+TMCSEHM@users.noreply.github.com>
2026-01-07 23:50:22 -0800

    Add ability to print a report:  word count, and sorted character count

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 # Import the 'get_num_words' function from the 'stats.py' file
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, convert_dict_to_list, sort_on, sort_dict
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
+    num_chars_list = convert_dict_to_list(num_chars)
+    sorted_char_num = sort_dict(num_chars_list)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_chars)
-
+    print("--------- Character Count -------")
+    
+    for item in sorted_char_num:
+        char = item["char"]
+        num = item["num"]
+        if char.isalpha():
+            print(f"{char}: {num}")
+    
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -3,12 +3,31 @@ def get_num_words(text):
     return len(words)
 
 def get_num_chars(text):
-    num_chars = {}
-    for char in text:
+    chars = {}
+    for c in text:
         # Check for *lowercase* version!
-        if char.lower() in num_chars:
-            num_chars[char.lower()] += 1
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            num_chars[char.lower()] = 1
-    return num_chars
+            chars[lowered] = 1
+    return chars
+
+def convert_dict_to_list(chars_dict):
+    result_list = []
+    for char, num in chars_dict.items():
+        new_dict = {
+                "char": char, 
+                "num": num
+                }
+        # .append here otherwise += makes a mess of it
+        result_list.append(new_dict)
+    return result_list
+
+def sort_on(chars_dict):
+    return chars_dict["num"]
+    
+def sort_dict(chars):
+    chars.sort(reverse=True, key=sort_on)
+    return chars
 
