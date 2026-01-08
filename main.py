3fccf5feab1148ca13aaca354e99b24c455d2490
TMCSEHM <92201012+TMCSEHM@users.noreply.github.com>
2026-01-08 00:22:42 -0800

    Clean up (refactor) code

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 import sys
 # Import the 'get_num_words', ... functions from the 'stats.py' file
-from stats import get_num_words, get_num_chars, convert_dict_to_list, sort_on, sort_dict
+from stats import (
+        get_num_words, 
+        get_num_chars, 
+        convert_dict_to_list, 
+        #sort_on, 
+        #sort_dict,
+        )
 
 def main():
-    if len(sys.argv) !=2:
+    if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
-    num_chars_list = convert_dict_to_list(num_chars)
-    sorted_char_num = sort_dict(num_chars_list)
+    sorted_chars_list = convert_dict_to_list(num_chars)
+    #sorted_char_num = sort_dict(num_chars_list)
+    print_report(book_path, num_words, sorted_chars_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, sorted_chars_list):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    
-    for item in sorted_char_num:
-        char = item["char"]
-        num = item["num"]
-        if char.isalpha():
-            print(f"{char}: {num}")
+
+    #for item in sorted_char_num:
+     #   char = item["char"]
+      #  num = item["num"]
+       # if char.isalpha():
+        #    print(f"{char}: {num}")
+
+    for item in sorted_chars_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item["char"]}: {item["num"]}")
     
     print("============= END ===============")
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
 
 
 main()
--- stats.py
+++ stats.py
@@ -13,21 +13,30 @@ def get_num_chars(text):
             chars[lowered] = 1
     return chars
 
-def convert_dict_to_list(chars_dict):
-    result_list = []
-    for char, num in chars_dict.items():
-        new_dict = {
-                "char": char, 
-                "num": num
-                }
-        # .append here otherwise += makes a mess of it
-        result_list.append(new_dict)
-    return result_list
+#def convert_dict_to_list(chars_dict):
+ #   result_list = []
+  #  for char, num in chars_dict.items():
+   #     new_dict = {
+    #            "char": char, 
+     #           "num": num
+      #          }
+        ## .append here otherwise += makes a mess of it
+       # result_list.append(new_dict)
+    #return result_list
 
 def sort_on(chars_dict):
     return chars_dict["num"]
     
-def sort_dict(chars):
-    chars.sort(reverse=True, key=sort_on)
-    return chars
+#def sort_dict(chars):
+ #   chars.sort(reverse=True, key=sort_on)
+  #  return chars
+
+
+# "Correct" way to do it, didn't learn .items yet
+def convert_dict_to_list(chars_dict):
+    sorted_list = []
+    for ch in chars_dict:
+        sorted_list.append({"char": ch, "num": chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
