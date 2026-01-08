0bc155abc40f3c14f6a96f2218d5bc81bb799030
TMCSEHM <92201012+TMCSEHM@users.noreply.github.com>
2026-01-07 21:21:54 -0800

    Clean up main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    num_words = string_to_words(text)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
     print(f"Found {num_words} total words")
 
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
 
-def string_to_words(file_contents):
-    num_word = 0
-    words = file_contents.split()
-    for i in range(0, len(words)):
-        num_word += 1
-    return num_word
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
 
 main()
+
