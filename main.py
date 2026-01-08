1eecb9b18685ce3470f795d600f23b6318f87f54
TMCSEHM <92201012+TMCSEHM@users.noreply.github.com>
2026-01-07 23:59:51 -0800

    Finished bookbot, integrated sys.argv to allow any book passed through argument instead of hardcoded frankenstein.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-# Import the 'get_num_words' function from the 'stats.py' file
+import sys
+# Import the 'get_num_words', ... functions from the 'stats.py' file
 from stats import get_num_words, get_num_chars, convert_dict_to_list, sort_on, sort_dict
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) !=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
